65ce867388fad59e2a139ccf4a690ee03ca43ae9
e10indika <indikaseeu@gmail.com>
2026-01-17 08:42:59 -0500

    reset master

--- modules/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Wine Quality Predictor - Modular ML Components
-"""
-
-from modules.data_preprocessing import DataPreprocessor
-from modules.model_training import ModelTrainer
-from modules.model_evaluation import ModelEvaluator
-
-__all__ = ['DataPreprocessor', 'ModelTrainer', 'ModelEvaluator']
